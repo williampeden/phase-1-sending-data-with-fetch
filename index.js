ac80d4b8b092807377387ba53264d2cac3e0ebca
William <wgepeden@gmail.com>
2022-02-15 22:01:46 -0500

    commit tree c44a57ffac2722189820e17e5310fec740940fab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject ={
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res =>res.json())
+    .then(body => newContact (body.id))
+
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
+
+function newContact(contact){
+    document.getElementsByTagName("body")[0].innerHTML = contact
+    document.body.append(contact)
+}
